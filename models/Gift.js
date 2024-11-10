2f5c10f4f7e126176b71a01180445fbb8c8dcf84
sohailkhattab <36365427+sohailkhattab@users.noreply.github.com>
2024-11-10 14:01:26 +0200

    Update Gift.js

--- models/Gift.js
+++ models/Gift.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const componentSchema = new mongoose.Schema({
     compType: { type: String, required: true }, // e.g., 'Accordion', 'Form', 'ButtonCard'
-    config: { type: mongoose.Schema.Types.Mixed, required: true }, // Store component configuration
+    config_en: { type: mongoose.Schema.Types.Mixed, required: true }, // Store component configuration
   });
 
 const resSchema = new mongoose.Schema({
@@ -20,7 +20,6 @@ const resSchema = new mongoose.Schema({
   // Export schema
   const giftSchema = new mongoose.Schema({
     name: { type: String, required: true }, // Name or identifier for the export
-    components: [componentSchema], // Array of components
     res: [resSchema], // Array of 'res' objects
     createdAt: { type: Date, default: Date.now },
   });
