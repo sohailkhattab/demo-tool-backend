0d346a397a21798454d26f17b7bffeb3520c5c42
sohailkhattab <36365427+sohailkhattab@users.noreply.github.com>
2024-11-10 14:01:51 +0200

    Update Export.js

--- models/Export.js
+++ models/Export.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 
 // Component schema for export
 const componentSchema = new mongoose.Schema({
-  compType: { type: String, required: true }, // e.g., 'Accordion', 'Form', 'ButtonCard'
-  config: { type: mongoose.Schema.Types.Mixed, required: true }, // Store component configuration
+  compType: { type: String }, // e.g., 'Accordion', 'Form', 'ButtonCard'
+  config_en: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {}
+  }, // Configuration for English
+  config_ar: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {}
+  }, // Configuration for Arabic
 });
+componentSchema.set('discriminatorKey', 'compType');
+
+
+
 
 const backgroundSchema = new mongoose.Schema({
   backgroundImage: { type: String},
@@ -14,23 +25,11 @@ const backgroundSchema = new mongoose.Schema({
   backgroundRepeat: { type: String},
 });
 
-// Schema for 'res' objects
-const resSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  lifecycleStatus: { type: String, required: true },
-  '@type': { type: String, required: true },
-  category: { type: String, required: true },
-  channel: { type: mongoose.Schema.Types.Mixed }, // Adjust based on actual data type
-  characteristics: [{ type: mongoose.Schema.Types.Mixed }], // Array of mixed types
-  pattern: [{ type: mongoose.Schema.Types.Mixed }], // Array of mixed types
-});
-
 // Export schema
 const exportSchema = new mongoose.Schema({
   name: { type: String, required: true }, // Name or identifier for the export
-  components: [componentSchema], // Array of components
-  res: [resSchema], // Array of 'res' objects
-  background: [backgroundSchema],
+  components: { type: Map, of: componentSchema, required: true }, // Object of components
+  background: { type: backgroundSchema, required: true }, // object of background
   createdAt: { type: Date, default: Date.now },
 });
 
